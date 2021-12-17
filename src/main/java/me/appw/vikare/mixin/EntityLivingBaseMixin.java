ad2b59e55456a93ee5750c8b1340b4221b5bb6e5
Appw0 <austinpaulpal@gmail.com>
2021-12-16 22:40:43 -0800

    Mixing in
    Forgot to push EntityLivingBaseMixin

---
+++ src/main/java/me/appw/vikare/mixin/EntityLivingBaseMixin.java
@@ -0,0 +1,27 @@
+package me.appw.vikare.mixin;
+
+import me.appw.vikare.core.ViCore;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(EntityLivingBase.class)
+public abstract class EntityLivingBaseMixin extends Entity {
+
+    @Shadow protected abstract boolean isPlayer();
+
+    public EntityLivingBaseMixin(World worldIn) {
+        super(worldIn);
+    }
+
+    @ModifyVariable(method = "updateElytra",
+            name = "flag",
+            at = @At(value = "STORE", ordinal = 2))
+    private boolean updateElytra_flag(boolean flag) {
+        return ViCore.hasWorkingWings((EntityLivingBase)(Object)this); // hehe funni cast
+    }
+}
