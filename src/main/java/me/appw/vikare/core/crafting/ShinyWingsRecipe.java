25da16d61a9a2fd73c7de4fab49efac8ac65c624
Appw0 <austinpaulpal@gmail.com>
2021-12-22 11:13:49 -0800

    Glowstone no longer hardcoded

--- src/main/java/me/appw/vikare/core/crafting/ShinyWingsRecipe.java
+++ src/main/java/me/appw/vikare/core/crafting/ShinyWingsRecipe.java
@@ -4,35 +4,38 @@ import me.appw.vikare.Vikare;
 import me.appw.vikare.common.items.WingItem;
 import me.appw.vikare.core.registry.RecipeSerializers;
 import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.SpecialRecipe;
+import net.minecraft.tags.ITag;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.common.Tags;
 
 public class ShinyWingsRecipe extends SpecialRecipe {
+    public static final ITag.INamedTag<Item> SHINE = ItemTags.makeWrapperTag(Vikare.MODID + ":shine");
+
     public ShinyWingsRecipe(ResourceLocation resourceLocation) { super(resourceLocation); }
 
     @Override
     public boolean matches(CraftingInventory inv, World world) {
         boolean hasWings = false;
-        boolean hasGlowstone = false;
+        boolean hasShiny = false;
         for (int i = 0; i < inv.getSizeInventory(); i++) {
             ItemStack stack = inv.getStackInSlot(i);
             if (!stack.isEmpty()) {
-                    if (stack.getItem() instanceof WingItem) {
-                        if (hasWings) return false;
-                        hasWings = true;
-                } else {
-                    if (Tags.Items.DUSTS_GLOWSTONE.contains(stack.getItem())) {
-                        if (hasGlowstone) return false;
-                        hasGlowstone = true;
-                    }
+                if (stack.getItem() instanceof WingItem) {
+                    if (hasWings) return false;
+                    hasWings = true;
+                } else if (SHINE.contains(stack.getItem())) {
+                    if (hasShiny) return false;
+                    hasShiny = true;
                 }
             }
         }
-        return hasWings && hasGlowstone;
+        return hasWings && hasShiny;
     }
 
     @Override
